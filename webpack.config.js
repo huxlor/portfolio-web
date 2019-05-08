ac0d77ffaa8c75a700d71978c29a5d7fa8c504fc
Sebastian Diaz <jsebasdc@gmail.com>
2019-05-08 15:20:21 -0500

    Landing Categorias Desktop Final

---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+    entry: './src/main.js',
+    output: {
+        path: path.resolve(__dirname, 'dist'),
+        filename: 'landing-categorias.js'
+    },
+    module: {
+        rules: [{
+            test: /\.(scss|css)$/,
+            use: [
+                MiniCssExtractPlugin.loader, // creates style nodes from JS strings
+                "css-loader", // translates CSS into CommonJS
+                "sass-loader" // compiles Sass to CSS, using Node Sass by default
+            ]
+        },{
+            test: /\.(eot|woff2|woff|ttf)$/,
+            use: [
+                {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 115000         
+                    }
+                }
+            ]
+        },{
+            test: /\.(jpg|png|svg)$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        name: '[name].[ext]'
+                    }
+                }
+            ]
+        }
+    ]
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'landing-categorias.css',
+            chunkFilename: 'id.css'
+        })
+    ]
+};
