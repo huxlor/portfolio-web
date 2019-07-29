c5c9e69e1384ee6021673e338f65957247333358
huxlor <jsebasdc@gmail.com>
2019-07-28 21:50:06 -0500

    Seccion de Projects grilla

--- src/main.js
+++ src/main.js
@@ -1,11 +1,26 @@
 require('bootstrap-grid');
 require('./styles.scss');
 
+let infoProyectos = require('./portfolio_info');
+
 
 (function($) {
 
 	jQuery(document).ready(function($) {
-        console.log('All is Working');
+
+        infoProyectos.forEach((element) => {
+            console.log(element.empresa);
+            $('.projects__container ul').append(`
+                <li style="background-image: url('${element.image}')">
+                    <div class="overlay">
+                        <div class="project">${element.proyecto}</div>
+                        <div class="rol">${element.rol}</div>
+                        <div class="empresa">${element.empresa}</div>
+                        <a href="${element.url}">View</a>
+                    </div>
+                </li>`);
+        });
+
     });
 
 
---
+++ src/portfolio_info.js
@@ -0,0 +1,30 @@
+module.exports = [
+    {
+        proyecto: "Fogacoop",
+        rol: "Build Front",
+        empresa: "Bextechnology",
+        url: "https://www.fogacoop.gov.co/",
+        image: "https://images.unsplash.com/photo-1459257831348-f0cdd359235f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+    },
+    {
+        proyecto: "Decameron",
+        rol: "Build Front",
+        empresa: "Bextechnology",
+        url: "https://www.decameron.com",
+        image: "https://images.unsplash.com/photo-1495954484750-af469f2f9be5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80"
+    },
+    {
+        proyecto: "Tiendas Jumbo",
+        rol: "Build Front - Realease",
+        empresa: "Cencosud SA",
+        url: "https://www.tiendasjumbo.co/",
+        image: "https://images.unsplash.com/photo-1516594798947-e65505dbb29d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=750&q=80"
+    },
+    {
+        proyecto: "Tiendas Metro",
+        rol: "Build Front - Realease",
+        empresa: "Cencosud SA",
+        url: "https://www.tiendasmetro.co/",
+        image: "https://images.unsplash.com/photo-1553546895-531931aa1aa8?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80"
+    }
+];
